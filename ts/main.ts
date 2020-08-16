346334db148d0b4ac7d53e9f03a1cb77106201eb
Kilian Gärtner <kilian.gaertner@naymspace.de>
2020-08-16 14:40:00 +0200

    Add basic about me section

--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,13 @@
+import differenceInYears from "date-fns/differenceInYears";
+
+// Empty module so that imports are working
+export {};
+
 (ready());
 
 function ready() {
   navBarOnClick();
+  calculateYears();
 }
 
 function navBarOnClick() {
@@ -14,3 +20,9 @@ function navBarOnClick() {
   });
 }
 
+function calculateYears() {
+  // Date I started working on minecraft addons
+  const start = new Date(2009, 1, 1);
+  const years = differenceInYears(Date.now(), start);
+  document.getElementById("years").innerText = years.toString();
+}
