f6882ced003cc1f6cb4f3486f087e6ffeeff2f47
Kilian Gärtner <kilian.gaertner@naymspace.de>
2020-08-16 11:40:14 +0200

    Implement smooth scroll to target

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+(ready());
+
+function ready() {
+  navBarOnClick();
+}
+
+function navBarOnClick() {
+  document.querySelectorAll("nav > a").forEach((ele) => {
+    ele.addEventListener('click', (el) => {
+      ele.scrollIntoView({
+        behavior: 'smooth'
+      })
+    });
+  });
+}
+
