030342449a7fd1952c5039f9cd6ce454e2bb108b
meldanor <bhaals@gmx.de>
2021-08-22 23:40:12 +0200

    Add basic description

---
+++ components/Description.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2 class="pt-2 text-4xl font-semibold">
+      Kilian Gärtner
+    </h2>
+    <h3 class="pt-2 text-2xl font-semibold1">
+      Software Entwickler bei
+      <a
+        class="text-green-500"
+        href="https://eves.de"
+        target="_blank"
+        rel="noreferrer"
+      >eves_it</a>
+    </h3>
+    <div class="pt-4 text-lg tracking-wide text-justify">
+      <p>
+        Als Software Ingenieur konzipiere und entwickele seit über 12 Jahren Software für verschiedenste Anwendungsbereiche.
+        Ich bin neuen Technologien sehr aufgeschlossen, bilde mich privat ständig weiter.
+        So kann ich dem Kunden einen geeigneten Tech Stack anbieten.
+      </p>
+      <p>
+        Es macht mir Spaß für andere Entwickler die Einrichtungshürden zu senken und die Entwicklungsprozesse ständig zu verbessern.
+      </p>
+      <p>
+        Dafür nutze ich die Ökosysteme aus Elixir und Java für die Backendentwicklung. Im Bereich DevOps ganz klar Docker
+        und CI/CDs verschiedenster Anbieter wie GitLab, Bamboo oder Jenkins.
+      </p>
+      <p>
+        Besonders interessant für mich sind Projekte mit Anforderungen an Skalierbarkeit und Performance.
+      </p>
+    </div>
+    <blockquote class="relative pt-8 text-xl italic">
+      <p class="mb-4">
+        Wer aufhört, besser zu werden, hat aufgehört, gut zu sein.
+      </p>
+      <cite>
+        Prof. Philip Rosentahl
+      </cite>
+    </blockquote>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <starter />
     <description />
   </div>
