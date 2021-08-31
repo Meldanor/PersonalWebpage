7363b032b597128b021285350c946724c92d0ee8
meldanor <bhaals@gmx.de>
2021-08-31 12:51:48 +0200

    Configure tailwind to be usable by IntelliJ IDEA
    Remove svg import - using img with src instead to remove dependency hell

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   head: {
     title: 'personal_website',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'de'
     },
     meta: [
       { charset: 'utf-8' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <starter />
+  <div>
+    <starter />
+    <description />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Description from '~/components/Description.vue'
 import Starter from '~/components/Starter.vue'
 
-export default Vue.extend({ components: { Starter } })
+export default Vue.extend({
+  components: {
+    Starter,
+    Description
+  }
+})
 </script>
