6b46dcc84dc979b3a5c1d1c26b58211aeaaaab97
meldanor <bhaals@gmx.de>
2023-07-18 22:24:52 +0200

    fix: make file download nicer

--- components/documents/OpenFile.vue
+++ components/documents/OpenFile.vue
@@ -1,6 +1,7 @@
 <template>
   <a class="open-file" target="_blank" rel="noreferrer" :href="url">
     {{ displayName }}
+    <img class="w-8 h-8" src="/logos/download-minimalistic-svgrepo-com.svg" alt="github logo">
   </a>
 </template>
 
@@ -14,10 +15,6 @@
 <style>
 .open-file {
   background-color: rgba(195, 224, 241, var(--tw-border-opacity));
-  @apply w-52 h-16 p-2 text-black font-semibold transition-colors flex items-center justify-center text-center m-2 lg:my-0
-}
-.open-file:hover {
-  --tw-bg-opacity: 1;
-  background-color: rgba(152, 202, 231, var(--tw-border-opacity));
+  @apply w-52 h-16 p-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-between
 }
 </style>
