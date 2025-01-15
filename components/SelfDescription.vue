fbbb1a5fc971b4fa25554963505c8313d3df4efd
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2025-01-15 18:25:19 +0100

    delete: old stuff

--- components/SelfDescription.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex flex-wrap w-full">
-    <description class="lg:w-1/2 w-full p-4 md:pr-5" />
-    <soft-skills class="lg:w-1/2 w-full md:pl-5" />
-  </div>
-</template>
